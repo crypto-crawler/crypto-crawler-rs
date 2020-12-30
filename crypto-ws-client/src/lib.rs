bc8aaf74814d860d33dbe70528f15a755db8af5b
soulmachine <soulmachine@gmail.com>
2020-12-29 17:51:26 -0800

    Fixed test

--- crypto-ws-client/src/lib.rs
+++ crypto-ws-client/src/lib.rs
@@ -6,7 +6,7 @@
 //! use crypto_ws_client::{BinanceSpotWSClient, WSClient};
 //!
 //! fn main() {
-//!     let mut ws_client = BinanceSpotWSClient::init(|msg| println!("{}", msg));
+//!     let mut ws_client = BinanceSpotWSClient::init(|msg| println!("{}", msg), None);
 //!     let channels = vec!["btcusdt@aggTrade".to_string(), "btcusdt@depth".to_string(),];
 //!     ws_client.subscribe(&channels);
 //!     ws_client.run();
