8651f154fa3c0cb547d631d033ed07f858a0405f
soulmachine <soulmachine@gmail.com>
2021-01-02 14:44:56 -0800

    impl Trade for BitMEX

--- crypto-ws-client/tests/bitmex.rs
+++ crypto-ws-client/tests/bitmex.rs
@@ -4,22 +4,48 @@ use crypto_ws_client::{BitMEXWSClient, WSClient};
 mod utils;
 
 #[test]
-fn bitmex_swap() {
-    gen_test_subscribe!(
-        BitMEXWSClient,
-        &vec!["trade:XBTUSD".to_string(), "quote:XBTUSD".to_string()]
-    );
-}
-
-#[test]
-fn bitmex_futures() {
-    gen_test_subscribe!(
-        BitMEXWSClient,
-        &vec!["trade:XBTM21".to_string(), "quote:XBTM21".to_string()]
-    );
-}
-
-#[test]
 fn bitmex_instrument() {
     gen_test_subscribe!(BitMEXWSClient, &vec!["instrument".to_string()]);
+}
+
+#[cfg(test)]
+mod bitmex_swap {
+    use crypto_ws_client::{BitMEXWSClient, Trade, WSClient};
+
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(
+            BitMEXWSClient,
+            &vec!["trade:XBTUSD".to_string(), "quote:XBTUSD".to_string()]
+        );
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(
+            BitMEXWSClient,
+            &vec!["XBTUSD".to_string(), "ETHUSD".to_string()]
+        );
+    }
+}
+
+#[cfg(test)]
+mod bitmex_future {
+    use crypto_ws_client::{BitMEXWSClient, Trade, WSClient};
+
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(
+            BitMEXWSClient,
+            &vec!["trade:XBTM21".to_string(), "quote:XBTM21".to_string()]
+        );
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(
+            BitMEXWSClient,
+            &vec!["XBTM21".to_string(), "ETHH21".to_string()]
+        );
+    }
 }
