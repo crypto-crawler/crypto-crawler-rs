fc4e7aea042fd7aabbd73b62e623ba3f594c0a00
soulmachine <soulmachine@gmail.com>
2021-01-02 13:53:51 -0800

    Organized Binance tests by market types

--- crypto-ws-client/tests/binance.rs
+++ crypto-ws-client/tests/binance.rs
@@ -1,39 +1,54 @@
-use crypto_ws_client::{
-    BinanceFutureWSClient, BinanceInverseSwapWSClient, BinanceLinearSwapWSClient,
-    BinanceSpotWSClient, WSClient,
-};
-
 #[macro_use]
 mod utils;
 
-#[test]
-fn binance_spot() {
-    gen_test_subscribe!(
-        BinanceSpotWSClient,
-        &vec!["btcusdt@aggTrade".to_string(), "btcusdt@ticker".to_string()]
-    );
+#[cfg(test)]
+mod spot {
+    use crypto_ws_client::{BinanceSpotWSClient, WSClient};
+
+    #[test]
+    fn test_subscribe() {
+        gen_test_subscribe!(
+            BinanceSpotWSClient,
+            &vec!["btcusdt@aggTrade".to_string(), "btcusdt@ticker".to_string()]
+        );
+    }
 }
 
-#[test]
-fn binance_futures() {
-    gen_test_subscribe!(
-        BinanceFutureWSClient,
-        &vec!["btcusd_210625@aggTrade".to_string()]
-    );
+#[cfg(test)]
+mod future {
+    use crypto_ws_client::{BinanceFutureWSClient, WSClient};
+
+    #[test]
+    fn test_subscribe() {
+        gen_test_subscribe!(
+            BinanceFutureWSClient,
+            &vec!["btcusd_210625@aggTrade".to_string()]
+        );
+    }
 }
 
-#[test]
-fn binance_inverse_swap() {
-    gen_test_subscribe!(
-        BinanceInverseSwapWSClient,
-        &vec!["btcusd_perp@aggTrade".to_string()]
-    );
+#[cfg(test)]
+mod inverse_swap {
+    use crypto_ws_client::{BinanceInverseSwapWSClient, WSClient};
+
+    #[test]
+    fn test_subscribe() {
+        gen_test_subscribe!(
+            BinanceInverseSwapWSClient,
+            &vec!["btcusd_perp@aggTrade".to_string()]
+        );
+    }
 }
 
-#[test]
-fn binance_linear_swap() {
-    gen_test_subscribe!(
-        BinanceLinearSwapWSClient,
-        &vec!["btcusdt@aggTrade".to_string()]
-    );
+#[cfg(test)]
+mod linear_swap {
+    use crypto_ws_client::{BinanceLinearSwapWSClient, WSClient};
+
+    #[test]
+    fn test_subscribe() {
+        gen_test_subscribe!(
+            BinanceLinearSwapWSClient,
+            &vec!["btcusdt@aggTrade".to_string()]
+        );
+    }
 }
