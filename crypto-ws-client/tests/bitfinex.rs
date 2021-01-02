c1e84b606318f689a290bbe511b9ddb8bea8340a
soulmachine <soulmachine@gmail.com>
2021-01-02 14:29:00 -0800

    impl Trade for Bitfinex

--- crypto-ws-client/tests/bitfinex.rs
+++ crypto-ws-client/tests/bitfinex.rs
@@ -1,14 +1,32 @@
-use crypto_ws_client::{BitfinexWSClient, WSClient};
-
 #[macro_use]
 mod utils;
 
-#[test]
-fn bitfinex_spot() {
-    gen_test_subscribe!(BitfinexWSClient, &vec!["trades:tBTCUST".to_string()]);
+#[cfg(test)]
+mod bitfinex_spot {
+    use crypto_ws_client::{BitfinexWSClient, Trade, WSClient};
+
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(BitfinexWSClient, &vec!["trades:tBTCUST".to_string()]);
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(BitfinexWSClient, &vec!["BTCUST".to_string()]);
+    }
 }
 
-#[test]
-fn bitfinex_swap() {
-    gen_test_subscribe!(BitfinexWSClient, &vec!["trades:tBTCF0:USTF0".to_string()]);
+#[cfg(test)]
+mod bitfinex_swap {
+    use crypto_ws_client::{BitfinexWSClient, Trade, WSClient};
+
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(BitfinexWSClient, &vec!["trades:tBTCF0:USTF0".to_string()]);
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(BitfinexWSClient, &vec!["BTCF0:USTF0".to_string()]);
+    }
 }
