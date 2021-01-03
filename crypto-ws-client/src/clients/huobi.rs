c5d491410f6193b3956b544ea2851fe04225daa2
soulmachine <soulmachine@gmail.com>
2021-01-02 18:34:11 -0800

    impl Trade for Huobi

--- crypto-ws-client/src/clients/huobi.rs
+++ crypto-ws-client/src/clients/huobi.rs
@@ -1,4 +1,4 @@
-use crate::WSClient;
+use crate::{Trade, WSClient};
 use std::collections::HashMap;
 
 use log::*;
@@ -19,110 +19,176 @@ const COIN_SWAP_WEBSOCKET_URL: &str = "wss://futures.huobi.com/swap-ws";
 const USDT_SWAP_WEBSOCKET_URL: &str = "wss://futures.huobi.com/linear-swap-ws";
 const OPTION_WEBSOCKET_URL: &str = "wss://futures.huobi.com/option-ws";
 
-/// The WebSocket client for Huobi Spot market(<https://huobiapi.github.io/docs/spot/v1/en/>).
-pub struct HuobiSpotWSClient<'a> {
+// Internal unified client
+struct HuobiWSClient<'a> {
     client: WSClientInternal<'a>,
 }
 
-/// The WebSocket client for Huobi Futures market(<https://huobiapi.github.io/docs/dm/v1/en/>).
-pub struct HuobiFuturesWSClient<'a> {
-    client: WSClientInternal<'a>,
-}
-
-/// The WebSocket client for Huobi Coin Swap market(<https://huobiapi.github.io/docs/coin_margined_swap/v1/en/>).
-pub struct HuobiCoinSwapWSClient<'a> {
-    client: WSClientInternal<'a>,
-}
-
-/// The WebSocket client for Huobi USDT Swap market(<https://huobiapi.github.io/docs/usdt_swap/v1/en/>).
-pub struct HuobiUsdtSwapWSClient<'a> {
-    client: WSClientInternal<'a>,
-}
-/// The WebSocket client for Huobi Option market(<https://huobiapi.github.io/docs/option/v1/en/>).
-pub struct HuobiOptionWSClient<'a> {
-    client: WSClientInternal<'a>,
-}
-
-fn channel_to_command(channel: &str, subscribe: bool) -> String {
-    format!(
-        r#"{{"{}":"{}","id":"crypto-ws-client"}}"#,
-        if subscribe { "sub" } else { "unsub" },
-        channel
-    )
-}
-
-fn channels_to_commands(channels: &[String], subscribe: bool) -> Vec<String> {
-    channels
-        .iter()
-        .map(|ch| channel_to_command(ch, subscribe))
-        .collect()
-}
-
-fn on_misc_msg(msg: &str) -> MiscMessage {
-    let resp = serde_json::from_str::<HashMap<String, Value>>(&msg);
-    if resp.is_err() {
-        error!("{} is not a JSON string, {}", msg, EXCHANGE_NAME);
-        return MiscMessage::Misc;
-    }
-    let obj = resp.unwrap();
-
-    if obj.contains_key("ping") {
-        let value = obj.get("ping").unwrap();
-        let mut pong_msg = HashMap::<String, &Value>::new();
-        pong_msg.insert("pong".to_string(), value);
-        let ws_msg = Message::Text(serde_json::to_string(&pong_msg).unwrap());
-        return MiscMessage::WebSocket(ws_msg);
-    }
-
-    if let Some(status) = obj.get("status") {
-        if status.as_str().unwrap() != "ok" {
-            error!("Received {} from {}", msg, EXCHANGE_NAME);
-            return MiscMessage::Misc;
+impl<'a> HuobiWSClient<'a> {
+    fn new(url: &str, on_msg: Box<dyn FnMut(String) + 'a>) -> Self {
+        HuobiWSClient {
+            client: WSClientInternal::new(
+                EXCHANGE_NAME,
+                url,
+                on_msg,
+                Self::on_misc_msg,
+                Self::channels_to_commands,
+            ),
         }
     }
 
-    if !obj.contains_key("ch") || !obj.contains_key("ts") {
-        warn!("Received {} from {}", msg, EXCHANGE_NAME);
-        return MiscMessage::Misc;
+    fn channel_to_command(channel: &str, subscribe: bool) -> String {
+        format!(
+            r#"{{"{}":"{}","id":"crypto-ws-client"}}"#,
+            if subscribe { "sub" } else { "unsub" },
+            channel
+        )
     }
-    MiscMessage::Normal
+
+    fn channels_to_commands(channels: &[String], subscribe: bool) -> Vec<String> {
+        channels
+            .iter()
+            .map(|ch| Self::channel_to_command(ch, subscribe))
+            .collect()
+    }
+
+    fn on_misc_msg(msg: &str) -> MiscMessage {
+        let resp = serde_json::from_str::<HashMap<String, Value>>(&msg);
+        if resp.is_err() {
+            error!("{} is not a JSON string, {}", msg, EXCHANGE_NAME);
+            return MiscMessage::Misc;
+        }
+        let obj = resp.unwrap();
+
+        if obj.contains_key("ping") {
+            let value = obj.get("ping").unwrap();
+            let mut pong_msg = HashMap::<String, &Value>::new();
+            pong_msg.insert("pong".to_string(), value);
+            let ws_msg = Message::Text(serde_json::to_string(&pong_msg).unwrap());
+            return MiscMessage::WebSocket(ws_msg);
+        }
+
+        if let Some(status) = obj.get("status") {
+            if status.as_str().unwrap() != "ok" {
+                error!("Received {} from {}", msg, EXCHANGE_NAME);
+                return MiscMessage::Misc;
+            }
+        }
+
+        if !obj.contains_key("ch") || !obj.contains_key("ts") {
+            warn!("Received {} from {}", msg, EXCHANGE_NAME);
+            return MiscMessage::Misc;
+        }
+        MiscMessage::Normal
+    }
 }
 
-define_client!(
-    HuobiSpotWSClient,
-    EXCHANGE_NAME,
-    SPOT_WEBSOCKET_URL,
-    channels_to_commands,
-    on_misc_msg
-);
-define_client!(
-    HuobiFuturesWSClient,
-    EXCHANGE_NAME,
-    FUTURES_WEBSOCKET_URL,
-    channels_to_commands,
-    on_misc_msg
-);
-define_client!(
-    HuobiCoinSwapWSClient,
-    EXCHANGE_NAME,
-    COIN_SWAP_WEBSOCKET_URL,
-    channels_to_commands,
-    on_misc_msg
-);
-define_client!(
-    HuobiUsdtSwapWSClient,
-    EXCHANGE_NAME,
-    USDT_SWAP_WEBSOCKET_URL,
-    channels_to_commands,
-    on_misc_msg
-);
-define_client!(
-    HuobiOptionWSClient,
-    EXCHANGE_NAME,
-    OPTION_WEBSOCKET_URL,
-    channels_to_commands,
-    on_misc_msg
-);
+impl<'a> Trade for HuobiWSClient<'a> {
+    fn subscribe_trade(&mut self, pairs: &[String]) {
+        let pair_to_raw_channel = |pair: &String| format!("market.{}.trade.detail", pair);
+
+        let channels = pairs
+            .iter()
+            .map(pair_to_raw_channel)
+            .collect::<Vec<String>>();
+        self.client.subscribe(&channels);
+    }
+}
+
+/// Huobi Spot market.
+///
+/// * WebSocket API doc: <https://huobiapi.github.io/docs/spot/v1/en/>
+/// * Trading at: <https://www.huobi.com/en-us/exchange/>
+pub struct HuobiSpotWSClient<'a> {
+    client: HuobiWSClient<'a>,
+}
+
+/// Huobi Future market.
+///
+/// * WebSocket API doc: <https://huobiapi.github.io/docs/dm/v1/en/>
+/// * Trading at: <https://futures.huobi.com/en-us/contract/exchange/>
+pub struct HuobiFutureWSClient<'a> {
+    client: HuobiWSClient<'a>,
+}
+
+/// Huobi Inverse Swap market.
+///
+/// Inverse Swap market uses coins like BTC as collateral.
+///
+/// * WebSocket API doc: <<https://huobiapi.github.io/docs/coin_margined_swap/v1/en/>>
+/// * Trading at: <https://futures.huobi.com/en-us/swap/exchange/>
+pub struct HuobiInverseSwapWSClient<'a> {
+    client: HuobiWSClient<'a>,
+}
+
+/// Huobi Linear Swap market.
+///
+/// Linear Swap market uses USDT as collateral.
+///
+/// * WebSocket API doc: <https://huobiapi.github.io/docs/usdt_swap/v1/en/>
+/// * Trading at: <https://futures.huobi.com/en-us/linear_swap/exchange/>
+pub struct HuobiLinearSwapWSClient<'a> {
+    client: HuobiWSClient<'a>,
+}
+/// Huobi Option market.
+///
+///
+/// * WebSocket API doc: <https://huobiapi.github.io/docs/option/v1/en/>
+/// * Trading at: <https://futures.huobi.com/en-us/option/exchange/>
+pub struct HuobiOptionWSClient<'a> {
+    client: HuobiWSClient<'a>,
+}
+
+/// Define market specific client.
+macro_rules! define_market_client {
+    ($struct_name:ident, $default_url:ident) => {
+        impl<'a> WSClient<'a> for $struct_name<'a> {
+            fn new(on_msg: Box<dyn FnMut(String) + 'a>, url: Option<&str>) -> Self {
+                let real_url = match url {
+                    Some(endpoint) => endpoint,
+                    None => $default_url,
+                };
+                $struct_name {
+                    client: HuobiWSClient::new(real_url, on_msg),
+                }
+            }
+
+            fn subscribe(&mut self, channels: &[String]) {
+                self.client.client.subscribe(channels);
+            }
+
+            fn unsubscribe(&mut self, channels: &[String]) {
+                self.client.client.unsubscribe(channels);
+            }
+
+            fn run(&mut self, duration: Option<u64>) {
+                self.client.client.run(duration);
+            }
+        }
+    };
+}
+
+define_market_client!(HuobiSpotWSClient, SPOT_WEBSOCKET_URL);
+define_market_client!(HuobiFutureWSClient, FUTURES_WEBSOCKET_URL);
+define_market_client!(HuobiInverseSwapWSClient, COIN_SWAP_WEBSOCKET_URL);
+define_market_client!(HuobiLinearSwapWSClient, USDT_SWAP_WEBSOCKET_URL);
+define_market_client!(HuobiOptionWSClient, OPTION_WEBSOCKET_URL);
+
+macro_rules! impl_trade {
+    ($struct_name:ident) => {
+        impl<'a> Trade for $struct_name<'a> {
+            fn subscribe_trade(&mut self, pairs: &[String]) {
+                self.client.subscribe_trade(pairs);
+            }
+        }
+    };
+}
+
+impl_trade!(HuobiSpotWSClient);
+impl_trade!(HuobiFutureWSClient);
+impl_trade!(HuobiInverseSwapWSClient);
+impl_trade!(HuobiLinearSwapWSClient);
+impl_trade!(HuobiOptionWSClient);
 
 #[cfg(test)]
 mod tests {
@@ -130,12 +196,12 @@ mod tests {
     fn test_channel_to_command() {
         assert_eq!(
             r#"{"sub":"market.btcusdt.trade.detail","id":"crypto-ws-client"}"#,
-            super::channel_to_command("market.btcusdt.trade.detail", true)
+            super::HuobiWSClient::channel_to_command("market.btcusdt.trade.detail", true)
         );
 
         assert_eq!(
             r#"{"unsub":"market.btcusdt.trade.detail","id":"crypto-ws-client"}"#,
-            super::channel_to_command("market.btcusdt.trade.detail", false)
+            super::HuobiWSClient::channel_to_command("market.btcusdt.trade.detail", false)
         );
     }
 }
--- crypto-ws-client/tests/huobi.rs
+++ crypto-ws-client/tests/huobi.rs
@@ -1,55 +1,111 @@
-use crypto_ws_client::{
-    HuobiCoinSwapWSClient, HuobiFuturesWSClient, HuobiOptionWSClient, HuobiSpotWSClient,
-    HuobiUsdtSwapWSClient, WSClient,
-};
-
 #[macro_use]
 mod utils;
 
-#[test]
-fn huobi_spot() {
-    gen_test_subscribe!(
-        HuobiSpotWSClient,
-        &vec!["market.btcusdt.trade.detail".to_string()]
-    );
+#[cfg(test)]
+mod huobi_spot {
+    use crypto_ws_client::{HuobiSpotWSClient, Trade, WSClient};
+
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(
+            HuobiSpotWSClient,
+            &vec!["market.btcusdt.trade.detail".to_string()]
+        );
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(HuobiSpotWSClient, &vec!["btcusdt".to_string()]);
+    }
+
+    #[test]
+    fn huobi_hb10() {
+        gen_test_subscribe!(
+            HuobiSpotWSClient,
+            &vec![
+                "market.hb10usdt.trade.detail".to_string(),
+                "market.huobi10.kline.1min".to_string()
+            ]
+        );
+    }
 }
 
-#[test]
-fn huobi_futures() {
-    gen_test_subscribe!(
-        HuobiFuturesWSClient,
-        &vec!["market.BTC_CQ.trade.detail".to_string()]
-    );
+#[cfg(test)]
+mod huobi_future {
+    use crypto_ws_client::{HuobiFutureWSClient, Trade, WSClient};
+
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(
+            HuobiFutureWSClient,
+            &vec!["market.BTC_CQ.trade.detail".to_string()]
+        );
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(HuobiFutureWSClient, &vec!["BTC_CQ".to_string()]);
+    }
 }
 
-#[test]
-fn huobi_usdt_swap() {
-    gen_test_subscribe!(
-        HuobiUsdtSwapWSClient,
-        &vec!["market.BTC-USDT.trade.detail".to_string()]
-    );
+#[cfg(test)]
+mod huobi_linear_swap {
+    use crypto_ws_client::{HuobiLinearSwapWSClient, Trade, WSClient};
+
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(
+            HuobiLinearSwapWSClient,
+            &vec!["market.BTC-USDT.trade.detail".to_string()]
+        );
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(HuobiLinearSwapWSClient, &vec!["BTC-USDT".to_string()]);
+    }
 }
 
-#[test]
-fn huobi_coin_swap() {
-    gen_test_subscribe!(
-        HuobiCoinSwapWSClient,
-        &vec!["market.BTC-USD.trade.detail".to_string()]
-    );
+#[cfg(test)]
+mod huobi_inverse_swap {
+    use crypto_ws_client::{HuobiInverseSwapWSClient, Trade, WSClient};
+
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(
+            HuobiInverseSwapWSClient,
+            &vec!["market.BTC-USD.trade.detail".to_string()]
+        );
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(HuobiInverseSwapWSClient, &vec!["BTC-USD".to_string()]);
+    }
 }
 
-#[test]
-fn huobi_option() {
-    gen_test_subscribe!(HuobiOptionWSClient, &vec!["market.overview".to_string()]);
-}
+#[cfg(test)]
+mod huobi_option {
+    use crypto_ws_client::{HuobiOptionWSClient, Trade, WSClient};
 
-#[test]
-fn huobi_hb10() {
-    gen_test_subscribe!(
-        HuobiSpotWSClient,
-        &vec![
-            "market.hb10usdt.trade.detail".to_string(),
-            "market.huobi10.kline.1min".to_string()
-        ]
-    );
+    #[test]
+    fn subscribe() {
+        gen_test_subscribe!(
+            HuobiOptionWSClient,
+            &vec!["market.BTC-USDT-210326-C-32000.trade.detail".to_string()]
+        );
+    }
+
+    #[test]
+    fn subscribe_trade() {
+        gen_test_subscribe_trade!(
+            HuobiOptionWSClient,
+            &vec!["BTC-USDT-210326-C-32000".to_string()]
+        );
+    }
+
+    #[test]
+    fn subscribe_overview() {
+        gen_test_subscribe!(HuobiOptionWSClient, &vec!["market.overview".to_string()]);
+    }
 }
