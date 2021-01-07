30dd8a5c48b46f886e3b024018ff745e719f4939
soulmachine <soulmachine@gmail.com>
2021-01-06 23:11:58 -0800

    cleanup

--- crypto-crawler/tests/utils/mod.rs
+++ crypto-crawler/tests/utils/mod.rs
@@ -1,16 +1,3 @@
-macro_rules! gen_test_subscribe {
-    ($client:ident, $channels:expr) => {
-        let mut messages = Vec::<String>::new();
-        {
-            let on_msg = |msg: String| messages.push(msg);
-            let mut ws_client = $client::new(Box::new(on_msg), None);
-            ws_client.subscribe($channels);
-            ws_client.run(Some(0)); // return immediately once after a normal message
-        }
-        assert!(!messages.is_empty());
-    };
-}
-
 macro_rules! gen_test_code {
     ($crawl_func:ident, $exchange:expr, $market_type:expr, $symbol:expr, $msg_type:expr) => {{
         let mut messages = Vec::<Message>::new();
