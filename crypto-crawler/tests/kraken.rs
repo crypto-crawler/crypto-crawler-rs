9983655e163d86581038aa7d68d0d6ff599e1236
soulmachine <soulmachine@gmail.com>
2021-01-07 11:53:59 -0800

    Kraken

---
+++ crypto-crawler/tests/kraken.rs
@@ -0,0 +1,37 @@
+use crypto_crawler::*;
+
+#[macro_use]
+mod utils;
+
+#[test]
+fn test_crawl_trade() {
+    gen_test_code!(
+        crawl_trade,
+        "Kraken",
+        MarketType::Spot,
+        "XBT/USD",
+        MessageType::Trade
+    )
+}
+
+#[test]
+fn test_crawl_l2_event() {
+    gen_test_code!(
+        crawl_l2_event,
+        "Kraken",
+        MarketType::Spot,
+        "XBT/USD",
+        MessageType::L2Event
+    )
+}
+
+#[test]
+fn test_crawl_l2_snapshot() {
+    gen_test_code!(
+        crawl_l2_snapshot,
+        "Kraken",
+        MarketType::Spot,
+        "XBT/USD",
+        MessageType::L2Snapshot
+    )
+}
