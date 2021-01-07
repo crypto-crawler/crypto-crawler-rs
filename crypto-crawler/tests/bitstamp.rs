9b808e973bd810b45ccf8fd5249082638312627d
soulmachine <soulmachine@gmail.com>
2021-01-07 02:00:33 -0800

    Implemented Bitstamp

---
+++ crypto-crawler/tests/bitstamp.rs
@@ -0,0 +1,59 @@
+use crypto_crawler::*;
+
+#[macro_use]
+mod utils;
+
+#[test]
+fn test_crawl_trade() {
+    gen_test_code!(
+        crawl_trade,
+        "Bitstamp",
+        MarketType::Spot,
+        "btcusd",
+        MessageType::Trade
+    )
+}
+
+#[test]
+fn test_crawl_l2_event() {
+    gen_test_code!(
+        crawl_l2_event,
+        "Bitstamp",
+        MarketType::Spot,
+        "btcusd",
+        MessageType::L2Event
+    )
+}
+
+#[test]
+fn test_crawl_l2_snapshot() {
+    gen_test_code!(
+        crawl_l2_snapshot,
+        "Bitstamp",
+        MarketType::Spot,
+        "btcusd",
+        MessageType::L2Snapshot
+    )
+}
+
+#[test]
+fn test_crawl_l3_event() {
+    gen_test_code!(
+        crawl_l3_event,
+        "Bitstamp",
+        MarketType::Spot,
+        "btcusd",
+        MessageType::L3Event
+    )
+}
+
+#[test]
+fn test_crawl_l3_snapshot() {
+    gen_test_code!(
+        crawl_l3_snapshot,
+        "Bitstamp",
+        MarketType::Spot,
+        "btcusd",
+        MessageType::L3Snapshot
+    )
+}
