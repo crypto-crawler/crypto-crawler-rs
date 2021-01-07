bb9b4138ba96bdf5dc56fa1ad92c3f2b05d97c5a
soulmachine <soulmachine@gmail.com>
2021-01-07 01:59:35 -0800

    Moved macro gen_crawl_snapshot to utils.rs

--- crypto-crawler/src/crawlers/coinbase_pro.rs
+++ crypto-crawler/src/crawlers/coinbase_pro.rs
@@ -88,81 +88,14 @@ pub(crate) fn crawl_l2_snapshot<'a>(
     duration: Option<u64>,
 ) {
     check_market_type(market_type);
-    let mut on_msg_ext = |json: String, symbol: String| {
-        let message = Message::new(
-            EXCHANGE_NAME.to_string(),
-            market_type,
-            symbol,
-            MessageType::L2Snapshot,
-            json,
-        );
-        on_msg(message);
-    };
-
-    let now = Instant::now();
-    loop {
-        let mut succeeded = false;
-        for symbol in symbols.iter() {
-            let resp = CoinbaseProRestClient::fetch_l2_snapshot(symbol);
-            match resp {
-                Ok(msg) => {
-                    on_msg_ext(msg, symbol.to_string());
-                    succeeded = true
-                }
-                Err(err) => error!(
-                    "{} {} {}, error: {}",
-                    EXCHANGE_NAME, market_type, symbol, err
-                ),
-            }
-        }
-
-        if let Some(seconds) = duration {
-            if now.elapsed() > Duration::from_secs(seconds) && succeeded {
-                break;
-            }
-        }
-    }
-}
-
-macro_rules! gen_crawl_snapshot {
-    ($market_type:ident, $symbols:ident, $on_msg:ident, $duration:ident, $msg_type:expr, $fetch_snapshot:expr) => {{
-        let mut on_msg_ext = |json: String, symbol: String| {
-            let message = Message::new(
-                EXCHANGE_NAME.to_string(),
-                $market_type,
-                symbol,
-                $msg_type,
-                json,
-            );
-            ($on_msg)(message);
-        };
-
-        let now = Instant::now();
-        loop {
-            let mut succeeded = false;
-            for symbol in $symbols.iter() {
-                let resp = ($fetch_snapshot)(symbol);
-                match resp {
-                    Ok(msg) => {
-                        on_msg_ext(msg, symbol.to_string());
-                        succeeded = true
-                    }
-                    Err(err) => error!(
-                        "{} {} {}, error: {}",
-                        EXCHANGE_NAME, $market_type, symbol, err
-                    ),
-                }
-            }
-
-            if let Some(seconds) = $duration {
-                if now.elapsed() > Duration::from_secs(seconds) && succeeded {
-                    break;
-                }
-            }
-
-            std::thread::sleep(Duration::from_secs(crate::SNAPSHOT_INTERVAL));
-        }
-    }};
+    gen_crawl_snapshot!(
+        market_type,
+        symbols,
+        on_msg,
+        duration,
+        MessageType::L2Snapshot,
+        CoinbaseProRestClient::fetch_l2_snapshot
+    )
 }
 
 pub(crate) fn crawl_l3_snapshot<'a>(
