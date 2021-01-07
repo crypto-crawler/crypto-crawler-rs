92467c87b6713369fadbfa3805d4f5bb80618abd
soulmachine <soulmachine@gmail.com>
2021-01-06 18:28:22 -0800

    Added l3 for CoinbasePro

--- crypto-ws-client/tests/coinbase_pro.rs
+++ crypto-ws-client/tests/coinbase_pro.rs
@@ -15,6 +15,12 @@ fn subscribe() {
 }
 
 #[test]
+#[should_panic]
+fn subscribe_illegal_symbol() {
+    gen_test_subscribe!(CoinbaseProWSClient, &vec!["matches:XXX-YYY".to_string(),]);
+}
+
+#[test]
 fn subscribe_raw_json() {
     gen_test_subscribe!(
         CoinbaseProWSClient,
@@ -58,4 +64,16 @@ fn subscribe_ticker() {
 #[test]
 fn subscribe_orderbook() {
     gen_test_subscribe_orderbook!(CoinbaseProWSClient, &vec!["BTC-USD".to_string()]);
+}
+
+#[test]
+fn subscribe_l3_orderbook() {
+    let mut messages = Vec::<String>::new();
+    {
+        let on_msg = |msg: String| messages.push(msg);
+        let mut ws_client = CoinbaseProWSClient::new(Box::new(on_msg), None);
+        ws_client.subscribe_orderbook(&vec!["BTC-USD".to_string()]);
+        ws_client.run(Some(0)); // return immediately once after a normal message
+    }
+    assert!(!messages.is_empty());
 }
