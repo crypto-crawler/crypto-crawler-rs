c722f93ee839aa57262187c349397f879e304859
soulmachine <soulmachine@gmail.com>
2021-01-09 06:14:21 -0800

    Made rust-analyzer not showing red errors

--- crypto-ws-client/tests/utils/mod.rs
+++ crypto-ws-client/tests/utils/mod.rs
@@ -1,14 +1,15 @@
 macro_rules! gen_test_code {
     ($client:ident, $func_name:ident, $pairs:expr) => {
-        let mut messages = Vec::<String>::new();
-        let on_msg = Rc::new(RefCell::new(|msg: String| messages.push(msg)));
+        let messages = Rc::new(RefCell::new(Vec::<String>::new()));
+        let messages2 = messages.clone();
+        let on_msg = Rc::new(RefCell::new(|msg: String| messages2.borrow_mut().push(msg)));
         {
             let mut ws_client = $client::new(on_msg.clone(), None);
             ws_client.$func_name($pairs);
             ws_client.run(Some(0)); // return immediately once after a normal message
             ws_client.close();
         }
-        assert!(!messages.is_empty());
+        assert!(!messages.borrow().is_empty());
     };
 }
 
@@ -16,14 +17,15 @@ macro_rules! gen_test_code {
 #[allow(unused_macros)]
 macro_rules! gen_test_subscribe_candlestick {
     ($client:ident, $pairs:expr, $interval:expr) => {
-        let mut messages = Vec::<String>::new();
-        let on_msg = Rc::new(RefCell::new(|msg: String| messages.push(msg)));
+        let messages = Rc::new(RefCell::new(Vec::<String>::new()));
+        let messages2 = messages.clone();
+        let on_msg = Rc::new(RefCell::new(|msg: String| messages2.borrow_mut().push(msg)));
         {
             let mut ws_client = $client::new(on_msg.clone(), None);
             ws_client.subscribe_candlestick($pairs, $interval);
             ws_client.run(Some(0)); // return immediately once after a normal message
             ws_client.close();
         }
-        assert!(!messages.is_empty());
+        assert!(!messages.borrow().is_empty());
     };
 }
