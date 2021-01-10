3adea48bbc9057d170240027cb2767582870322d
soulmachine <soulmachine@gmail.com>
2021-01-10 08:17:35 -0800

    cleanup

--- crypto-ws-client/tests/utils/mod.rs
+++ crypto-ws-client/tests/utils/mod.rs
@@ -1,17 +1,14 @@
 macro_rules! gen_test_code {
     ($client:ident, $func_name:ident, $pairs:expr) => {
-        let messages = Arc::new(Mutex::new(Vec::<String>::new()));
-        let messages2 = messages.clone();
-        let on_msg = Arc::new(Mutex::new(|msg: String| {
-            messages2.lock().unwrap().push(msg)
-        }));
+        let mut messages = Vec::<String>::new();
         {
+            let on_msg = Arc::new(Mutex::new(|msg: String| messages.push(msg)));
             let ws_client = $client::new(on_msg.clone(), None);
             ws_client.$func_name($pairs);
             ws_client.run(Some(0)); // return immediately once after a normal message
             ws_client.close();
         }
-        assert!(!messages.lock().unwrap().is_empty());
+        assert!(!messages.is_empty());
     };
 }
 
@@ -19,17 +16,14 @@ macro_rules! gen_test_code {
 #[allow(unused_macros)]
 macro_rules! gen_test_subscribe_candlestick {
     ($client:ident, $pairs:expr, $interval:expr) => {
-        let messages = Arc::new(Mutex::new(Vec::<String>::new()));
-        let messages2 = messages.clone();
-        let on_msg = Arc::new(Mutex::new(|msg: String| {
-            messages2.lock().unwrap().push(msg)
-        }));
+        let mut messages = Vec::<String>::new();
         {
+            let on_msg = Arc::new(Mutex::new(|msg: String| messages.push(msg)));
             let ws_client = $client::new(on_msg.clone(), None);
             ws_client.subscribe_candlestick($pairs, $interval);
             ws_client.run(Some(0)); // return immediately once after a normal message
             ws_client.close();
         }
-        assert!(!messages.lock().unwrap().is_empty());
+        assert!(!messages.is_empty());
     };
 }
