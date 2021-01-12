02b2d927db2a23d1aca0ad9e4716fb675df87617
soulmachine <soulmachine@gmail.com>
2021-01-12 00:03:00 -0800

    Implemented fetch_symbols() for Huobi

--- crypto-rest-client/src/exchanges/huobi/huobi_linear_swap.rs
+++ crypto-rest-client/src/exchanges/huobi/huobi_linear_swap.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 use super::super::utils::http_get;
 use crate::error::Result;
 use std::collections::HashMap;
@@ -16,3 +18,29 @@ pub struct HuobiLinearSwapRestClient {
 }
 
 impl_contract!(HuobiLinearSwapRestClient);
+
+impl HuobiLinearSwapRestClient {
+    /// Get active trading symbols.
+    pub fn fetch_symbols() -> Result<Vec<String>> {
+        let text = gen_api!("https://api.hbdm.com/linear-swap-api/v1/swap_contract_info")?;
+        let obj = serde_json::from_str::<HashMap<String, Value>>(&text).unwrap();
+        if obj.get("status").unwrap().as_str().unwrap() != "ok" {
+            return Err(crate::Error(text));
+        }
+
+        let arr = obj.get("data").unwrap().as_array().unwrap();
+        let symbols = arr
+            .into_iter()
+            .map(|v| v.as_object().unwrap())
+            .filter(|obj| obj.get("contract_status").unwrap().as_i64().unwrap() == 1)
+            .map(|obj| {
+                obj.get("contract_code")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string()
+            })
+            .collect::<Vec<String>>();
+        Ok(symbols)
+    }
+}
