87809280b5be683d033d726f76801695d11c1bb1
soulmachine <soulmachine@gmail.com>
2021-01-13 23:48:25 -0800

    Implemented fetch_symbols() of Bitfinex

---
+++ crypto-markets/tests/bitfinex.rs
@@ -0,0 +1,19 @@
+use crypto_markets::{fetch_symbols, MarketType};
+
+#[test]
+fn fetch_spot_symbols() {
+    let symbols = fetch_symbols("bitfinex", MarketType::Spot).unwrap();
+    assert!(!symbols.is_empty());
+    for symbol in symbols.iter() {
+        println!("{}", symbol);
+    }
+}
+
+#[test]
+fn fetch_linear_swap_symbols() {
+    let symbols = fetch_symbols("bitfinex", MarketType::LinearSwap).unwrap();
+    assert!(!symbols.is_empty());
+    for symbol in symbols.iter() {
+        assert!(symbol.ends_with("F0:USTF0") || symbol.ends_with("F0:BTCF0"));
+    }
+}
