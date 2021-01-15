684cd9f5d7c0e1e5f8bfcf2cf111a330bb18d91d
soulmachine <soulmachine@gmail.com>
2021-01-14 16:52:55 -0800

    typo

--- crypto-markets/tests/bitstamp.rs
+++ crypto-markets/tests/bitstamp.rs
@@ -2,6 +2,6 @@ use crypto_markets::{fetch_symbols, MarketType};
 
 #[test]
 fn fetch_spot_symbols() {
-    let symbols = fetch_symbols("binance", MarketType::Spot).unwrap();
+    let symbols = fetch_symbols("bitstamp", MarketType::Spot).unwrap();
     assert!(!symbols.is_empty());
 }
