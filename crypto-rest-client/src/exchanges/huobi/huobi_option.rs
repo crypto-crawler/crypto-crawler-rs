f46461302de86681d693b7118a38c6522e745bba
soulmachine <soulmachine@gmail.com>
2021-01-15 01:50:06 -0800

    Removed fetch_symbols() since it is implemented by crypto-markets

--- crypto-rest-client/src/exchanges/huobi/huobi_option.rs
+++ crypto-rest-client/src/exchanges/huobi/huobi_option.rs
@@ -1,5 +1,3 @@
-use serde_json::Value;
-
 use super::super::utils::http_get;
 use crate::error::Result;
 use std::collections::HashMap;
@@ -19,30 +17,6 @@ pub struct HuobiOptionRestClient {
 impl_contract!(HuobiOptionRestClient);
 
 impl HuobiOptionRestClient {
-    /// Get active trading symbols.
-    pub fn fetch_symbols() -> Result<Vec<String>> {
-        let text = gen_api!("https://api.hbdm.com/option-api/v1/option_contract_info")?;
-        let obj = serde_json::from_str::<HashMap<String, Value>>(&text).unwrap();
-        if obj.get("status").unwrap().as_str().unwrap() != "ok" {
-            return Err(crate::Error(text));
-        }
-
-        let arr = obj.get("data").unwrap().as_array().unwrap();
-        let symbols = arr
-            .into_iter()
-            .map(|v| v.as_object().unwrap())
-            .filter(|obj| obj.get("contract_status").unwrap().as_i64().unwrap() == 1)
-            .map(|obj| {
-                obj.get("contract_code")
-                    .unwrap()
-                    .as_str()
-                    .unwrap()
-                    .to_string()
-            })
-            .collect::<Vec<String>>();
-        Ok(symbols)
-    }
-
     /// Get the latest Level2 orderbook snapshot.
     ///
     /// Top 150 bids and asks (aggregated) are returned.
--- crypto-rest-client/tests/huobi.rs
+++ crypto-rest-client/tests/huobi.rs
@@ -5,12 +5,6 @@ mod huobi_spot {
     use std::collections::HashMap;
 
     #[test]
-    fn test_fetch_symbols() {
-        let symbols = HuobiSpotRestClient::fetch_symbols().unwrap();
-        assert!(!symbols.is_empty());
-    }
-
-    #[test]
     fn test_trades() {
         let text = HuobiSpotRestClient::fetch_trades("btcusdt").unwrap();
         assert!(text.starts_with("{"));
@@ -52,20 +46,6 @@ mod huobi_future {
     use crypto_rest_client::HuobiFutureRestClient;
     use serde_json::Value;
     use std::collections::HashMap;
-
-    #[test]
-    fn test_fetch_symbols() {
-        let symbols = HuobiFutureRestClient::fetch_symbols().unwrap();
-        assert!(!symbols.is_empty());
-        for symbol in symbols.iter() {
-            assert!(
-                symbol.ends_with("_CW")
-                    || symbol.ends_with("_NW")
-                    || symbol.ends_with("_CQ")
-                    || symbol.ends_with("_NQ")
-            );
-        }
-    }
 
     #[test]
     fn test_trades() {
@@ -111,15 +91,6 @@ mod huobi_linear_swap {
     use std::collections::HashMap;
 
     #[test]
-    fn test_fetch_symbols() {
-        let symbols = HuobiLinearSwapRestClient::fetch_symbols().unwrap();
-        assert!(!symbols.is_empty());
-        for symbol in symbols.iter() {
-            assert!(symbol.ends_with("-USDT"));
-        }
-    }
-
-    #[test]
     fn test_trades() {
         let text = HuobiLinearSwapRestClient::fetch_trades("BTC-USDT").unwrap();
         assert!(text.starts_with("{"));
@@ -163,15 +134,6 @@ mod huobi_inverse_swap {
     use std::collections::HashMap;
 
     #[test]
-    fn test_fetch_symbols() {
-        let symbols = HuobiInverseSwapRestClient::fetch_symbols().unwrap();
-        assert!(!symbols.is_empty());
-        for symbol in symbols.iter() {
-            assert!(symbol.ends_with("-USD"));
-        }
-    }
-
-    #[test]
     fn test_trades() {
         let text = HuobiInverseSwapRestClient::fetch_trades("BTC-USD").unwrap();
         assert!(text.starts_with("{"));
@@ -213,15 +175,6 @@ mod huobi_option {
     use crypto_rest_client::HuobiOptionRestClient;
     use serde_json::Value;
     use std::collections::HashMap;
-
-    #[test]
-    fn test_fetch_symbols() {
-        let symbols = HuobiOptionRestClient::fetch_symbols().unwrap();
-        assert!(!symbols.is_empty());
-        for symbol in symbols.iter() {
-            assert!(symbol.contains("-C-") || symbol.contains("-P-"));
-        }
-    }
 
     #[test]
     fn test_trades() {
