b81f842da7e6bc60531e53e34358bdcbd62759a2
soulmachine <soulmachine@gmail.com>
2021-01-14 16:53:51 -0800

    Implemented fetch_symbols() of CoinbasePro

---
+++ crypto-markets/tests/coinbase_pro.rs
@@ -0,0 +1,10 @@
+use crypto_markets::{fetch_symbols, MarketType};
+
+#[test]
+fn fetch_spot_symbols() {
+    let symbols = fetch_symbols("coinbase_pro", MarketType::Spot).unwrap();
+    assert!(!symbols.is_empty());
+    for symbol in symbols.iter() {
+        println!("{}", symbol);
+    }
+}
