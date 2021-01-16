ceb1f133731aeb8fd5a87ac98ab001abea5384aa
soulmachine <soulmachine@gmail.com>
2021-01-15 18:02:28 -0800

    Tested all market types

--- crypto-markets/tests/huobi.rs
+++ crypto-markets/tests/huobi.rs
@@ -1,14 +1,24 @@
-use crypto_markets::{fetch_symbols, MarketType};
+use crypto_markets::{fetch_symbols, get_market_types, MarketType};
+
+#[macro_use]
+mod utils;
+
+const EXCHANGE_NAME: &str = "huobi";
+
+#[test]
+fn fetch_all_symbols() {
+    gen_all_symbols!();
+}
 
 #[test]
 fn fetch_spot_symbols() {
-    let symbols = fetch_symbols("huobi", MarketType::Spot).unwrap();
+    let symbols = fetch_symbols(EXCHANGE_NAME, MarketType::Spot).unwrap();
     assert!(!symbols.is_empty());
 }
 
 #[test]
 fn fetch_inverse_future_symbols() {
-    let symbols = fetch_symbols("huobi", MarketType::InverseFuture).unwrap();
+    let symbols = fetch_symbols(EXCHANGE_NAME, MarketType::InverseFuture).unwrap();
     assert!(!symbols.is_empty());
     for symbol in symbols.iter() {
         assert!(
@@ -22,7 +32,7 @@ fn fetch_inverse_future_symbols() {
 
 #[test]
 fn fetch_inverse_swap_symbols() {
-    let symbols = fetch_symbols("huobi", MarketType::InverseSwap).unwrap();
+    let symbols = fetch_symbols(EXCHANGE_NAME, MarketType::InverseSwap).unwrap();
     assert!(!symbols.is_empty());
     for symbol in symbols.iter() {
         assert!(symbol.ends_with("-USD"));
@@ -31,7 +41,7 @@ fn fetch_inverse_swap_symbols() {
 
 #[test]
 fn fetch_linear_swap_symbols() {
-    let symbols = fetch_symbols("huobi", MarketType::LinearSwap).unwrap();
+    let symbols = fetch_symbols(EXCHANGE_NAME, MarketType::LinearSwap).unwrap();
     assert!(!symbols.is_empty());
     for symbol in symbols.iter() {
         assert!(symbol.ends_with("-USDT"));
@@ -40,7 +50,7 @@ fn fetch_linear_swap_symbols() {
 
 #[test]
 fn fetch_option_symbols() {
-    let symbols = fetch_symbols("huobi", MarketType::Option).unwrap();
+    let symbols = fetch_symbols(EXCHANGE_NAME, MarketType::Option).unwrap();
     assert!(!symbols.is_empty());
     for symbol in symbols.iter() {
         assert!(symbol.contains("-C-") || symbol.contains("-P-"));
