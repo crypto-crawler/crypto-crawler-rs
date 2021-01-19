8bb7592703f7237bcd44ac38ee6b891fc74d7715
soulmachine <soulmachine@gmail.com>
2021-01-18 18:41:35 -0800

    Tested system proxy

--- crypto-markets/src/exchanges/utils.rs
+++ crypto-markets/src/exchanges/utils.rs
@@ -23,3 +23,27 @@ pub(super) fn http_get(url: &str, params: Option<&HashMap<String, String>>) -> R
 
     Ok(response)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use serde_json::Value;
+
+    // System proxies are enabled by default, see <https://docs.rs/reqwest/latest/reqwest/#proxies>
+    #[test]
+    fn use_system_socks_proxy() {
+        std::env::set_var("https_proxy", "socks5://127.0.0.1:9050");
+        let text = super::http_get("https://check.torproject.org/api/ip", None).unwrap();
+        let obj = serde_json::from_str::<HashMap<String, Value>>(&text).unwrap();
+        assert!(obj.get("IsTor").unwrap().as_bool().unwrap());
+    }
+
+    #[test]
+    fn use_system_https_proxy() {
+        std::env::set_var("https_proxy", "http://127.0.0.1:8118");
+        let text = super::http_get("https://check.torproject.org/api/ip", None).unwrap();
+        let obj = serde_json::from_str::<HashMap<String, Value>>(&text).unwrap();
+        assert!(obj.get("IsTor").unwrap().as_bool().unwrap());
+    }
+}
