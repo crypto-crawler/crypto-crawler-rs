e94be3ceedd6e0a9f8f9dcddc4a0bcbbc3448e55
soulmachine <soulmachine@gmail.com>
2021-01-21 21:55:42 -0800

    Made read_message() timeout after 5 seconds so that auto_ping() can have a chance to acquire the lock

--- crypto-ws-client/src/clients/utils.rs
+++ crypto-ws-client/src/clients/utils.rs
@@ -1,10 +1,82 @@
-use std::{thread, time};
-
+use http::Uri;
 use log::*;
-use tungstenite::{client::AutoStream, WebSocket};
+use native_tls::{HandshakeError as TlsHandshakeError, TlsConnector};
+use std::{
+    net::{SocketAddr, TcpStream, ToSocketAddrs},
+    thread,
+    time::{self, Duration},
+};
+use tungstenite::{
+    client::{self, AutoStream, IntoClientRequest},
+    handshake::{client::Response, HandshakeError},
+    stream::{Mode, NoDelay, Stream as StreamSwitcher},
+    Error, Result, WebSocket,
+};
 
+fn wrap_stream(stream: TcpStream, domain: &str, mode: Mode) -> Result<AutoStream> {
+    match mode {
+        Mode::Plain => Ok(StreamSwitcher::Plain(stream)),
+        Mode::Tls => {
+            let connector = TlsConnector::builder().build()?;
+            connector
+                .connect(domain, stream)
+                .map_err(|e| match e {
+                    TlsHandshakeError::Failure(f) => f.into(),
+                    TlsHandshakeError::WouldBlock(_) => {
+                        panic!("Bug: TLS handshake not blocked")
+                    }
+                })
+                .map(StreamSwitcher::Tls)
+        }
+    }
+}
+
+// copied from https://github.com/snapview/tungstenite-rs/blob/master/src/client.rs#L167
+fn connect_to_some(addrs: &[SocketAddr], uri: &Uri, mode: Mode) -> Result<AutoStream> {
+    let domain = uri
+        .host()
+        .ok_or_else(|| Error::Url("No host name in the URL".into()))?;
+    for addr in addrs {
+        debug!("Trying to contact {} at {}...", uri, addr);
+        if let Ok(raw_stream) = TcpStream::connect(addr) {
+            let _ = raw_stream.set_read_timeout(Some(Duration::from_secs(5)));
+            if let Ok(stream) = wrap_stream(raw_stream, domain, mode) {
+                return Ok(stream);
+            }
+        }
+    }
+    Err(Error::Url(format!("Unable to connect to {}", uri).into()))
+}
+
+// Usually ws_stream.read_message() blocks forever,
+// with this function, it returns after 5 seconds if no data comming in
+fn connect_with_timeout(url: &str) -> Result<(WebSocket<AutoStream>, Response)> {
+    let request = url.into_client_request()?;
+
+    let uri = request.uri();
+    let mode = client::uri_mode(uri)?;
+    let host = request
+        .uri()
+        .host()
+        .ok_or_else(|| Error::Url("No host name in the URL".into()))?;
+    let port = uri.port_u16().unwrap_or(match mode {
+        Mode::Plain => 80,
+        Mode::Tls => 443,
+    });
+    let addrs = (host, port).to_socket_addrs()?;
+    let mut stream = connect_to_some(addrs.as_slice(), &request.uri(), mode)?;
+    NoDelay::set_nodelay(&mut stream, true)?;
+
+    client::client(request, stream).map_err(|e| match e {
+        HandshakeError::Failure(f) => f,
+        HandshakeError::Interrupted(_) => panic!("Bug: blocking handshake not blocked"),
+    })
+}
+
+// This function is equivalent to tungstenite::connect(), with an additional benefit that
+// it can make read_message() timeout after 5 seconds
 pub(super) fn connect_with_retry(url: &str) -> WebSocket<AutoStream> {
-    let mut res = tungstenite::connect(url);
+    let mut res = connect_with_timeout(url);
     let mut count: i8 = 1;
     while res.is_err() && count < 3 {
         warn!(
