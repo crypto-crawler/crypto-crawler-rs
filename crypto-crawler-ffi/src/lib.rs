50162a3ca286ab4b2da8c80e37b6a971e9ad7785
soulmachine <soulmachine@gmail.com>
2021-02-01 23:38:12 -0800

    Refactored code

--- crypto-crawler-ffi/src/lib.rs
+++ crypto-crawler-ffi/src/lib.rs
@@ -19,6 +19,36 @@ pub enum MarketType {
     QuantoSwap,
 }
 
+impl MarketType {
+    // Converts C MarketType to Rust MarketType
+    pub fn to_rust(&self) -> crypto_crawler::MarketType {
+        match *self {
+            MarketType::Spot => crypto_crawler::MarketType::Spot,
+            MarketType::LinearFuture => crypto_crawler::MarketType::LinearFuture,
+            MarketType::InverseFuture => crypto_crawler::MarketType::InverseFuture,
+            MarketType::LinearSwap => crypto_crawler::MarketType::LinearSwap,
+            MarketType::InverseSwap => crypto_crawler::MarketType::InverseSwap,
+            MarketType::Option => crypto_crawler::MarketType::Option,
+            MarketType::QuantoFuture => crypto_crawler::MarketType::QuantoFuture,
+            MarketType::QuantoSwap => crypto_crawler::MarketType::QuantoSwap,
+        }
+    }
+
+    // Converts Rust MarketType to C MarketType
+    pub fn from_rust(market_type: crypto_crawler::MarketType) -> Self {
+        match market_type {
+            crypto_crawler::MarketType::Spot => MarketType::Spot,
+            crypto_crawler::MarketType::LinearFuture => MarketType::LinearFuture,
+            crypto_crawler::MarketType::InverseFuture => MarketType::InverseFuture,
+            crypto_crawler::MarketType::LinearSwap => MarketType::LinearSwap,
+            crypto_crawler::MarketType::InverseSwap => MarketType::InverseSwap,
+            crypto_crawler::MarketType::Option => MarketType::Option,
+            crypto_crawler::MarketType::QuantoFuture => MarketType::QuantoFuture,
+            crypto_crawler::MarketType::QuantoSwap => MarketType::QuantoSwap,
+        }
+    }
+}
+
 /// The type of a message
 #[repr(C)]
 pub enum MessageType {
@@ -30,6 +60,22 @@ pub enum MessageType {
     BBO,
     Ticker,
     Candlestick,
+}
+
+impl MessageType {
+    // Converts Rust MessageType to C MessageType
+    pub fn from_rust(msg_type: crypto_crawler::MessageType) -> Self {
+        match msg_type {
+            crypto_crawler::MessageType::Trade => MessageType::Trade,
+            crypto_crawler::MessageType::L2Event => MessageType::L2Event,
+            crypto_crawler::MessageType::L2Snapshot => MessageType::L2Snapshot,
+            crypto_crawler::MessageType::L3Event => MessageType::L3Event,
+            crypto_crawler::MessageType::L3Snapshot => MessageType::L3Snapshot,
+            crypto_crawler::MessageType::BBO => MessageType::BBO,
+            crypto_crawler::MessageType::Ticker => MessageType::Ticker,
+            crypto_crawler::MessageType::Candlestick => MessageType::Candlestick,
+        }
+    }
 }
 
 /// Message represents messages received by crawlers.
@@ -49,58 +95,21 @@ pub struct Message {
     pub json: *const c_char,
 }
 
-fn convert_market_type_c_to_rust(market_type: MarketType) -> crypto_crawler::MarketType {
-    match market_type {
-        MarketType::Spot => crypto_crawler::MarketType::Spot,
-        MarketType::LinearFuture => crypto_crawler::MarketType::LinearFuture,
-        MarketType::InverseFuture => crypto_crawler::MarketType::InverseFuture,
-        MarketType::LinearSwap => crypto_crawler::MarketType::LinearSwap,
-        MarketType::InverseSwap => crypto_crawler::MarketType::InverseSwap,
-        MarketType::Option => crypto_crawler::MarketType::Option,
-        MarketType::QuantoFuture => crypto_crawler::MarketType::QuantoFuture,
-        MarketType::QuantoSwap => crypto_crawler::MarketType::QuantoSwap,
-    }
-}
+impl Message {
+    // Converts a Rust Message to a C Message struct.
+    pub fn from_rust(msg: crypto_crawler::Message) -> Self {
+        let exchange_cstring = CString::new(msg.exchange).unwrap();
+        let symbol_cstring = CString::new(msg.symbol).unwrap();
+        let json_cstring = CString::new(msg.json).unwrap();
 
-fn convert_market_type_rust_to_c(market_type: crypto_crawler::MarketType) -> MarketType {
-    match market_type {
-        crypto_crawler::MarketType::Spot => MarketType::Spot,
-        crypto_crawler::MarketType::LinearFuture => MarketType::LinearFuture,
-        crypto_crawler::MarketType::InverseFuture => MarketType::InverseFuture,
-        crypto_crawler::MarketType::LinearSwap => MarketType::LinearSwap,
-        crypto_crawler::MarketType::InverseSwap => MarketType::InverseSwap,
-        crypto_crawler::MarketType::Option => MarketType::Option,
-        crypto_crawler::MarketType::QuantoFuture => MarketType::QuantoFuture,
-        crypto_crawler::MarketType::QuantoSwap => MarketType::QuantoSwap,
-    }
-}
-
-fn convert_msg_type_rust_to_c(msg_type: crypto_crawler::MessageType) -> MessageType {
-    match msg_type {
-        crypto_crawler::MessageType::Trade => MessageType::Trade,
-        crypto_crawler::MessageType::L2Event => MessageType::L2Event,
-        crypto_crawler::MessageType::L2Snapshot => MessageType::L2Snapshot,
-        crypto_crawler::MessageType::L3Event => MessageType::L3Event,
-        crypto_crawler::MessageType::L3Snapshot => MessageType::L3Snapshot,
-        crypto_crawler::MessageType::BBO => MessageType::BBO,
-        crypto_crawler::MessageType::Ticker => MessageType::Ticker,
-        crypto_crawler::MessageType::Candlestick => MessageType::Candlestick,
-    }
-}
-
-// Converts a Rust Message to a C Message struct.
-fn convert_msg(msg: crypto_crawler::Message) -> Message {
-    let exchange_cstring = CString::new(msg.exchange).unwrap();
-    let symbol_cstring = CString::new(msg.symbol).unwrap();
-    let json_cstring = CString::new(msg.json).unwrap();
-
-    Message {
-        exchange: exchange_cstring.as_ptr(),
-        market_type: convert_market_type_rust_to_c(msg.market_type),
-        msg_type: convert_msg_type_rust_to_c(msg.msg_type),
-        symbol: symbol_cstring.as_ptr(),
-        received_at: msg.received_at,
-        json: json_cstring.as_ptr(),
+        Message {
+            exchange: exchange_cstring.as_ptr(),
+            market_type: MarketType::from_rust(msg.market_type),
+            msg_type: MessageType::from_rust(msg.msg_type),
+            symbol: symbol_cstring.as_ptr(),
+            received_at: msg.received_at,
+            json: json_cstring.as_ptr(),
+        }
     }
 }
 
@@ -149,13 +158,13 @@ pub extern "C" fn crawl_trade(
     let symbols_rust = convert_symbols(symbols, num_symbols);
 
     let on_msg_ext = Arc::new(Mutex::new(move |msg: crypto_crawler::Message| {
-        let msg_ffi = convert_msg(msg);
+        let msg_ffi = Message::from_rust(msg);
         on_msg(&msg_ffi);
     }));
 
     crypto_crawler::crawl_trade(
         exchange_rust,
-        convert_market_type_c_to_rust(market_type),
+        market_type.to_rust(),
         if symbols_rust.is_empty() {
             None
         } else {
@@ -165,7 +174,6 @@ pub extern "C" fn crawl_trade(
         if duration > 0 { Some(duration) } else { None },
     );
 }
-
 
 /// Crawl level2 orderbook update events.
 #[no_mangle]
@@ -186,13 +194,13 @@ pub extern "C" fn crawl_l2_event(
     let symbols_rust = convert_symbols(symbols, num_symbols);
 
     let on_msg_ext = Arc::new(Mutex::new(move |msg: crypto_crawler::Message| {
-        let msg_ffi = convert_msg(msg);
+        let msg_ffi = Message::from_rust(msg);
         on_msg(&msg_ffi);
     }));
 
     crypto_crawler::crawl_l2_event(
         exchange_rust,
-        convert_market_type_c_to_rust(market_type),
+        market_type.to_rust(),
         if symbols_rust.is_empty() {
             None
         } else {
@@ -223,13 +231,13 @@ pub extern "C" fn crawl_l2_snapshot(
     let symbols_rust = convert_symbols(symbols, num_symbols);
 
     let on_msg_ext = Arc::new(Mutex::new(move |msg: crypto_crawler::Message| {
-        let msg_ffi = convert_msg(msg);
+        let msg_ffi = Message::from_rust(msg);
         on_msg(&msg_ffi);
     }));
 
     crypto_crawler::crawl_l2_snapshot(
         exchange_rust,
-        convert_market_type_c_to_rust(market_type),
+        market_type.to_rust(),
         if symbols_rust.is_empty() {
             None
         } else {
@@ -260,13 +268,13 @@ pub extern "C" fn crawl_l3_event(
     let symbols_rust = convert_symbols(symbols, num_symbols);
 
     let on_msg_ext = Arc::new(Mutex::new(move |msg: crypto_crawler::Message| {
-        let msg_ffi = convert_msg(msg);
+        let msg_ffi = Message::from_rust(msg);
         on_msg(&msg_ffi);
     }));
 
     crypto_crawler::crawl_l3_event(
         exchange_rust,
-        convert_market_type_c_to_rust(market_type),
+        market_type.to_rust(),
         if symbols_rust.is_empty() {
             None
         } else {
@@ -297,13 +305,13 @@ pub extern "C" fn crawl_l3_snapshot(
     let symbols_rust = convert_symbols(symbols, num_symbols);
 
     let on_msg_ext = Arc::new(Mutex::new(move |msg: crypto_crawler::Message| {
-        let msg_ffi = convert_msg(msg);
+        let msg_ffi = Message::from_rust(msg);
         on_msg(&msg_ffi);
     }));
 
     crypto_crawler::crawl_l3_snapshot(
         exchange_rust,
-        convert_market_type_c_to_rust(market_type),
+        market_type.to_rust(),
         if symbols_rust.is_empty() {
             None
         } else {
