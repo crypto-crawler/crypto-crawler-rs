b2bc3fea44de902cf0556215c959b455432aa19f
soulmachine <soulmachine@gmail.com>
2021-08-31 00:40:46 -0700

    Fixed conf/logrotate.misc.conf

--- carbonbot/conf/upload.sh
+++ carbonbot/conf/upload.sh
@@ -8,8 +8,8 @@ market_type=$1
 # Infinite while loop
 while :
 do
+  sleep 60
   # Find files older than 15 minutes
   find $DATA_DIR/$market_type -name "*.json" -type f -mmin +15 | xargs -r pigz
   rclone move $DATA_DIR/$market_type $AWS_S3_DIR/$market_type --include '*.json.gz' --no-traverse --min-age 15m
-  sleep 60
 done
