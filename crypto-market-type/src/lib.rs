666113afd016431016f634f928e956d9bc0bee80
soulmachine <soulmachine@gmail.com>
2021-09-30 05:08:27 -0700

    Added Unknown

--- crypto-market-type/src/lib.rs
+++ crypto-market-type/src/lib.rs
@@ -27,6 +27,7 @@ use strum_macros::{Display, EnumString};
 #[serde(rename_all = "snake_case")]
 #[strum(serialize_all = "snake_case")]
 pub enum MarketType {
+    Unknown,
     Spot,
     LinearFuture,
     InverseFuture,
