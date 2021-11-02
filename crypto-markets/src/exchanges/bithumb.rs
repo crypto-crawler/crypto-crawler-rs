2a32142b7319be75b50bbf4308ee398be2efb3df
soulmachine <soulmachine@gmail.com>
2021-11-02 01:46:57 -0700

    Implemented fetch_markets() for bithumb

--- crypto-markets/src/exchanges/bithumb.rs
+++ crypto-markets/src/exchanges/bithumb.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use super::utils::http_get;
 use crate::{
     error::{Error, Result},
-    Market, MarketType,
+    Fees, Market, MarketType, Precision,
 };
 
 use serde::{Deserialize, Serialize};
@@ -16,8 +16,11 @@ pub(crate) fn fetch_symbols(market_type: MarketType) -> Result<Vec<String>> {
     }
 }
 
-pub(crate) fn fetch_markets(_market_type: MarketType) -> Result<Vec<Market>> {
-    Ok(Vec::new())
+pub(crate) fn fetch_markets(market_type: MarketType) -> Result<Vec<Market>> {
+    match market_type {
+        MarketType::Spot => fetch_spot_markets(),
+        _ => panic!("Unsupported market_type: {}", market_type),
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -91,4 +94,52 @@ fn fetch_spot_symbols() -> Result<Vec<String>> {
         .map(|m| m.symbol)
         .collect::<Vec<String>>();
     Ok(symbols)
+}
+
+fn fetch_spot_markets() -> Result<Vec<Market>> {
+    let markets = fetch_spot_coing()?
+        .spotConfig
+        .into_iter()
+        .map(|m| {
+            let info = serde_json::to_value(&m)
+                .unwrap()
+                .as_object()
+                .unwrap()
+                .clone();
+            let pair = crypto_pair::normalize_pair(&m.symbol, "bithumb").unwrap();
+            let (base, quote) = {
+                let v: Vec<&str> = pair.split('/').collect();
+                (v[0].to_string(), v[1].to_string())
+            };
+            let (base_id, quote_id) = {
+                let v: Vec<&str> = m.symbol.split('-').collect();
+                (v[0].to_string(), v[1].to_string())
+            };
+            Market {
+                exchange: "bithumb".to_string(),
+                market_type: MarketType::Spot,
+                symbol: m.symbol,
+                base_id,
+                quote_id,
+                base,
+                quote,
+                active: true,
+                margin: true,
+                // see https://www.bitglobal.com/en-us/fee
+                fees: Fees {
+                    maker: 0.001,
+                    taker: 0.001,
+                },
+                precision: Precision {
+                    tick_size: 1.0 / (10_i64.pow(m.accuracy[0].parse::<u32>().unwrap()) as f64),
+                    lot_size: 1.0 / (10_i64.pow(m.accuracy[1].parse::<u32>().unwrap()) as f64),
+                },
+                quantity_limit: None,
+                contract_value: None,
+                delivery_date: None,
+                info,
+            }
+        })
+        .collect::<Vec<Market>>();
+    Ok(markets)
 }
