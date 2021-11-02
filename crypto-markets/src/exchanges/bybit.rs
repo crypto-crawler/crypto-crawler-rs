125087b407a9cf1dc2f594fb2c06b3340f85e722
soulmachine <soulmachine@gmail.com>
2021-11-02 02:56:42 -0700

    Implemented fetch_markets() for bybit

--- crypto-markets/src/exchanges/bybit.rs
+++ crypto-markets/src/exchanges/bybit.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 
 use super::utils::http_get;
-use crate::{error::Result, Market, MarketType};
+use crate::{error::Result, Fees, Market, MarketType, Precision, QuantityLimit};
 
+use chrono::DateTime;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -15,8 +16,13 @@ pub(crate) fn fetch_symbols(market_type: MarketType) -> Result<Vec<String>> {
     }
 }
 
-pub(crate) fn fetch_markets(_market_type: MarketType) -> Result<Vec<Market>> {
-    Ok(Vec::new())
+pub(crate) fn fetch_markets(market_type: MarketType) -> Result<Vec<Market>> {
+    match market_type {
+        MarketType::InverseSwap => fetch_inverse_swap_markets(),
+        MarketType::LinearSwap => fetch_linear_swap_markets(),
+        MarketType::InverseFuture => fetch_inverse_future_markets(),
+        _ => panic!("Unsupported market_type: {}", market_type),
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -44,6 +50,7 @@ struct LotSizeFilter {
 struct BybitMarket {
     name: String,
     alias: String,
+    status: String,
     base_currency: String,
     quote_currency: String,
     price_scale: i64,
@@ -70,7 +77,11 @@ fn fetch_markets_raw() -> Result<Vec<BybitMarket>> {
     let txt = http_get("https://api.bybit.com/v2/public/symbols", None)?;
     let resp = serde_json::from_str::<Response>(&txt)?;
     assert_eq!(resp.ret_code, 0);
-    Ok(resp.result)
+    Ok(resp
+        .result
+        .into_iter()
+        .filter(|m| m.status == "Trading")
+        .collect())
 }
 
 fn fetch_inverse_swap_symbols() -> Result<Vec<String>> {
@@ -100,4 +111,93 @@ fn fetch_inverse_future_symbols() -> Result<Vec<String>> {
         .map(|m| m.name)
         .collect::<Vec<String>>();
     Ok(symbols)
+}
+
+fn to_market(raw_market: &BybitMarket) -> Market {
+    let pair = crypto_pair::normalize_pair(&raw_market.name, "bybit").unwrap();
+    let (base, quote) = {
+        let v: Vec<&str> = pair.split('/').collect();
+        (v[0].to_string(), v[1].to_string())
+    };
+    let delivery_date: Option<u64> = if (&raw_market.name[(raw_market.name.len() - 2)..])
+        .parse::<i64>()
+        .is_ok()
+    {
+        let n = raw_market.alias.len();
+        let s = raw_market.alias.as_str();
+        let month = &s[(n - 4)..(n - 2)];
+        let day = &s[(n - 2)..];
+        Some(
+            DateTime::parse_from_rfc3339(format!("2021-{}-{}T00:00:00+00:00", month, day).as_str())
+                .unwrap()
+                .timestamp_millis() as u64,
+        )
+    } else {
+        None
+    };
+    Market {
+        exchange: "bybit".to_string(),
+        market_type: if raw_market.name == raw_market.alias {
+            MarketType::InverseFuture
+        } else if raw_market.quote_currency == "USDT" {
+            MarketType::LinearSwap
+        } else {
+            MarketType::InverseSwap
+        },
+        symbol: raw_market.name.to_string(),
+        base_id: raw_market.base_currency.to_string(),
+        quote_id: raw_market.quote_currency.to_string(),
+        base,
+        quote,
+        active: raw_market.status == "Trading",
+        margin: true,
+        fees: Fees {
+            maker: raw_market.maker_fee.parse::<f64>().unwrap(),
+            taker: raw_market.taker_fee.parse::<f64>().unwrap(),
+        },
+        precision: Precision {
+            tick_size: raw_market.price_filter.tick_size.parse::<f64>().unwrap(),
+            lot_size: raw_market.lot_size_filter.qty_step,
+        },
+        quantity_limit: Some(QuantityLimit {
+            min: raw_market.lot_size_filter.min_trading_qty,
+            max: Some(raw_market.lot_size_filter.max_trading_qty as f64),
+        }),
+        contract_value: Some(1.0),
+        delivery_date,
+        info: serde_json::to_value(raw_market)
+            .unwrap()
+            .as_object()
+            .unwrap()
+            .clone(),
+    }
+}
+
+fn fetch_inverse_swap_markets() -> Result<Vec<Market>> {
+    let markets = fetch_markets_raw()?
+        .into_iter()
+        .filter(|m| m.name == m.alias && m.quote_currency == "USD")
+        .map(|m| to_market(&m))
+        .collect::<Vec<Market>>();
+    Ok(markets)
+}
+
+fn fetch_linear_swap_markets() -> Result<Vec<Market>> {
+    let markets = fetch_markets_raw()?
+        .into_iter()
+        .filter(|m| m.name == m.alias && m.quote_currency == "USDT")
+        .map(|m| to_market(&m))
+        .collect::<Vec<Market>>();
+    Ok(markets)
+}
+
+fn fetch_inverse_future_markets() -> Result<Vec<Market>> {
+    let markets = fetch_markets_raw()?
+        .into_iter()
+        .filter(|m| {
+            m.quote_currency == "USD" && (&m.name[(m.name.len() - 2)..]).parse::<i64>().is_ok()
+        })
+        .map(|m| to_market(&m))
+        .collect::<Vec<Market>>();
+    Ok(markets)
 }
--- crypto-markets/tests/bybit.rs
+++ crypto-markets/tests/bybit.rs
@@ -1,4 +1,4 @@
-use crypto_markets::{fetch_symbols, get_market_types, MarketType};
+use crypto_markets::{fetch_markets, fetch_symbols, get_market_types, MarketType};
 
 #[macro_use]
 mod utils;
@@ -36,4 +36,55 @@ fn fetch_inverse_future_symbols() {
         let date = &symbol[(symbol.len() - 2)..];
         assert!(date.parse::<i64>().is_ok());
     }
+}
+
+#[test]
+fn fetch_inverse_swap_markets() {
+    let markets = fetch_markets(EXCHANGE_NAME, MarketType::InverseSwap).unwrap();
+    assert!(!markets.is_empty());
+
+    let btcusd = markets
+        .iter()
+        .find(|m| m.symbol == "BTCUSD")
+        .unwrap()
+        .clone();
+    assert_eq!(btcusd.precision.tick_size, 0.5);
+    assert_eq!(btcusd.precision.lot_size, 1.0);
+    let quantity_limit = btcusd.quantity_limit.unwrap();
+    assert_eq!(quantity_limit.min, 1.0);
+    assert_eq!(quantity_limit.max, Some(1000000.0));
+}
+
+#[test]
+fn fetch_linear_swap_markets() {
+    let markets = fetch_markets(EXCHANGE_NAME, MarketType::LinearSwap).unwrap();
+    assert!(!markets.is_empty());
+
+    let btcusdt = markets
+        .iter()
+        .find(|m| m.symbol == "BTCUSDT")
+        .unwrap()
+        .clone();
+    assert_eq!(btcusdt.precision.tick_size, 0.5);
+    assert_eq!(btcusdt.precision.lot_size, 0.001);
+    let quantity_limit = btcusdt.quantity_limit.unwrap();
+    assert_eq!(quantity_limit.min, 0.001);
+    assert_eq!(quantity_limit.max, Some(100.0));
+}
+
+#[test]
+fn fetch_inverse_future_markets() {
+    let markets = fetch_markets(EXCHANGE_NAME, MarketType::InverseFuture).unwrap();
+    assert!(!markets.is_empty());
+
+    let btcusd = markets
+        .iter()
+        .find(|m| m.symbol == "BTCUSDZ21")
+        .unwrap()
+        .clone();
+    assert_eq!(btcusd.precision.tick_size, 0.5);
+    assert_eq!(btcusd.precision.lot_size, 1.0);
+    let quantity_limit = btcusd.quantity_limit.unwrap();
+    assert_eq!(quantity_limit.min, 1.0);
+    assert_eq!(quantity_limit.max, Some(1000000.0));
 }
