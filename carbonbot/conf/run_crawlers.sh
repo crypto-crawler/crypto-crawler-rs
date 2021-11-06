490667a280c6060f3d930ba5499c61362101e82e
soulmachine <soulmachine@gmail.com>
2021-11-06 20:41:13 +0000

    typo

--- carbonbot/conf/run_crawlers.sh
+++ carbonbot/conf/run_crawlers.sh
@@ -13,7 +13,7 @@ docker rm $(docker ps -aq --filter "name=carbonbot")
 msg_types=("trade" "l2_event" "l2_topk" "l3_event" "bbo" "ticker" "candlestick" "funding_rate" "other")
 
 for msg_type in ${msg_types[@]}; do
-  docker run -d --name carbonbot-$msg_type --restart always -v $LOCAL_DATA_DIR/data -e DATA_DIR=/data -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_S3_DIR=$AWS_S3_DIR -u "$(id -u):$(id -g)" soulmachine/carbonbot pm2-runtime start pm2.$msg_type.config.js
+  docker run -d --name carbonbot-$msg_type --restart always -v $LOCAL_DATA_DIR:/data -e DATA_DIR=/data -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_S3_DIR=$AWS_S3_DIR -u "$(id -u):$(id -g)" soulmachine/carbonbot pm2-runtime start pm2.$msg_type.config.js
 done
 
 docker system prune -af
