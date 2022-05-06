ab01a594e662ffc43244b5db70a89cf6bc989000
soulmachine <soulmachine@gmail.com>
2022-05-06 11:22:34 -0700

    Added TSV utilities

--- crypto-crawler/src/msg.rs
+++ crypto-crawler/src/msg.rs
@@ -3,6 +3,7 @@ use crypto_msg_type::MessageType;
 use serde::{Deserialize, Serialize};
 use std::{
     convert::TryInto,
+    str::FromStr,
     time::{SystemTime, UNIX_EPOCH},
 };
 
@@ -39,6 +40,29 @@ impl Message {
                 .try_into()
                 .unwrap(),
             json,
+        }
+    }
+
+    /// Convert to a TSV string.
+    ///
+    /// The `exchange`, `market_type` and `msg_type` fields are not included to save some disk space.
+    pub fn to_tsv_string(&self) -> String {
+        format!("{}\t{}", self.received_at, self.json)
+    }
+
+    /// Convert from a TSV string.
+    pub fn from_tsv_string(exchange: &str, market_type: &str, msg_type: &str, s: &str) -> Self {
+        let v: Vec<&str> = s.split('\t').collect();
+        assert_eq!(2, v.len());
+        let market_type = MarketType::from_str(market_type).unwrap();
+        let msg_type = MessageType::from_str(msg_type).unwrap();
+
+        Message {
+            exchange: exchange.to_string(),
+            market_type,
+            msg_type,
+            received_at: v[0].parse::<u64>().unwrap(),
+            json: v[1].to_string(),
         }
     }
 }
