0de8c7506571dfb4c1aa7d36699188b5dc70a9c4
soulmachine <soulmachine@gmail.com>
2022-05-31 14:52:08 -0700

    cargo upgrade

--- crypto-crawler/tests/gate.rs
+++ crypto-crawler/tests/gate.rs
@@ -11,18 +11,18 @@ use utils::parse;
 const EXCHANGE_NAME: &str = "gate";
 
 #[test_case(MarketType::Spot)]
-// #[test_case(MarketType::InverseSwap)]
-// #[test_case(MarketType::LinearSwap)]
-// #[test_case(MarketType::InverseFuture; "ignore")]
-// #[test_case(MarketType::LinearFuture; "ignore")]
+#[test_case(MarketType::InverseSwap)]
+#[test_case(MarketType::LinearSwap)]
+#[test_case(MarketType::InverseFuture)]
+#[test_case(MarketType::LinearFuture)]
 #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
 async fn test_crawl_trade_all(market_type: MarketType) {
     test_all_symbols!(crawl_trade, EXCHANGE_NAME, market_type, MessageType::Trade)
 }
 
 #[test_case(MarketType::Spot, "BTC_USDT")]
-// #[test_case(MarketType::InverseSwap, "BTC_USD")]
-// #[test_case(MarketType::LinearSwap, "BTC_USDT")]
+#[test_case(MarketType::InverseSwap, "BTC_USD")]
+#[test_case(MarketType::LinearSwap, "BTC_USDT")]
 // #[test_case(MarketType::InverseFuture, "BTC_USD_20220624"; "ignore")]
 // #[test_case(MarketType::LinearFuture, "BTC_USDT_20220624"; "ignore")]
 #[tokio::test(flavor = "multi_thread")]
@@ -37,10 +37,10 @@ async fn test_crawl_trade(market_type: MarketType, symbol: &str) {
 }
 
 #[test_case(MarketType::Spot, "BTC_USDT")]
-// #[test_case(MarketType::InverseSwap, "BTC_USD")]
-// #[test_case(MarketType::LinearSwap, "BTC_USDT")]
-// #[test_case(MarketType::InverseFuture, "BTC_USD_20220624"; "ignore")]
-// #[test_case(MarketType::LinearFuture, "BTC_USDT_20220624"; "ignore")]
+#[test_case(MarketType::InverseSwap, "BTC_USD")]
+#[test_case(MarketType::LinearSwap, "BTC_USDT")]
+#[test_case(MarketType::InverseFuture, "BTC_USD_20220624")]
+#[test_case(MarketType::LinearFuture, "BTC_USDT_20220624")]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_crawl_l2_event(market_type: MarketType, symbol: &str) {
     test_one_symbol!(
@@ -53,8 +53,8 @@ async fn test_crawl_l2_event(market_type: MarketType, symbol: &str) {
 }
 
 #[test_case(MarketType::Spot, "BTC_USDT")]
-// #[test_case(MarketType::InverseSwap, "BTC_USD")]
-// #[test_case(MarketType::LinearSwap, "BTC_USDT")]
+#[test_case(MarketType::InverseSwap, "BTC_USD")]
+#[test_case(MarketType::LinearSwap, "BTC_USDT")]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_crawl_bbo(market_type: MarketType, symbol: &str) {
     test_one_symbol!(
@@ -67,10 +67,10 @@ async fn test_crawl_bbo(market_type: MarketType, symbol: &str) {
 }
 
 #[test_case(MarketType::Spot, "BTC_USDT")]
-// #[test_case(MarketType::InverseSwap, "BTC_USD")]
-// #[test_case(MarketType::LinearSwap, "BTC_USDT")]
-// #[test_case(MarketType::InverseFuture, "BTC_USD_20220624"; "ignore")]
-// #[test_case(MarketType::LinearFuture, "BTC_USDT_20220624"; "ignore")]
+#[test_case(MarketType::InverseSwap, "BTC_USD")]
+#[test_case(MarketType::LinearSwap, "BTC_USDT")]
+#[test_case(MarketType::InverseFuture, "BTC_USD_20220624")]
+#[test_case(MarketType::LinearFuture, "BTC_USDT_20220624")]
 fn test_crawl_l2_snapshot(market_type: MarketType, symbol: &str) {
     test_crawl_restful!(
         crawl_l2_snapshot,
@@ -82,9 +82,10 @@ fn test_crawl_l2_snapshot(market_type: MarketType, symbol: &str) {
 }
 
 #[test_case(MarketType::Spot)]
-// #[test_case(MarketType::InverseSwap)]
-// #[test_case(MarketType::LinearSwap)]
-// #[test_case(MarketType::LinearFuture)]
+#[test_case(MarketType::InverseSwap)]
+#[test_case(MarketType::LinearSwap)]
+#[test_case(MarketType::InverseFuture)]
+#[test_case(MarketType::LinearFuture)]
 fn test_crawl_l2_snapshot_without_symbol(market_type: MarketType) {
     test_crawl_restful_all_symbols!(
         crawl_l2_snapshot,
@@ -95,10 +96,10 @@ fn test_crawl_l2_snapshot_without_symbol(market_type: MarketType) {
 }
 
 #[test_case(MarketType::Spot, "BTC_USDT")]
-// #[test_case(MarketType::InverseSwap, "BTC_USD")]
-// #[test_case(MarketType::LinearSwap, "BTC_USDT")]
-// #[test_case(MarketType::InverseFuture, "BTC_USD_20220624"; "ignore")]
-// #[test_case(MarketType::LinearFuture, "BTC_USDT_20220624"; "ignore")]
+#[test_case(MarketType::InverseSwap, "BTC_USD")]
+#[test_case(MarketType::LinearSwap, "BTC_USDT")]
+#[test_case(MarketType::InverseFuture, "BTC_USD_20220624")]
+#[test_case(MarketType::LinearFuture, "BTC_USDT_20220624")]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_crawl_ticker(market_type: MarketType, symbol: &str) {
     test_one_symbol!(
@@ -110,11 +111,11 @@ async fn test_crawl_ticker(market_type: MarketType, symbol: &str) {
     )
 }
 
-// #[test_case(MarketType::Spot)] // TODO: gate spot candlestick channel doesn't send any messages
 #[test_case(MarketType::Spot; "inconclusive due to too many symbols")]
-// #[test_case(MarketType::InverseSwap)]
-// #[test_case(MarketType::LinearSwap)]
-// #[test_case(MarketType::LinearFuture; "ignored because linear_future takes too long")]
+#[test_case(MarketType::InverseSwap)]
+#[test_case(MarketType::LinearSwap)]
+#[test_case(MarketType::InverseFuture)]
+#[test_case(MarketType::LinearFuture)]
 #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
 async fn test_crawl_candlestick(market_type: MarketType) {
     gen_test_crawl_candlestick!(EXCHANGE_NAME, market_type)
