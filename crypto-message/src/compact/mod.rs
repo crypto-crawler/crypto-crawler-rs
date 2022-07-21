d1cfd622c73deb5ea2232b62a1fc05a0992e8aa1
soulmachine <soulmachine@gmail.com>
2022-07-20 20:36:17 -0700

    Exported Float

--- crypto-message/src/compact/mod.rs
+++ crypto-message/src/compact/mod.rs
@@ -2,4 +2,4 @@ mod message;
 mod order;
 
 pub use message::{calculate_hash, BboMsg, Exchange, Message, OrderBookMsg, TickerMsg, TradeMsg};
-pub use order::{Order, QuantityChoice};
+pub use order::{Float, Order, QuantityChoice};
