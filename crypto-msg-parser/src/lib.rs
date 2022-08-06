05b372dba8d8738d23ef7fdb44f4124fb0733b6c
Frank Dai <soulmachine@gmail.com>
2022-08-05 19:19:34 -0700

    Update lib.rs

--- crypto-msg-parser/src/lib.rs
+++ crypto-msg-parser/src/lib.rs
@@ -212,7 +212,7 @@ pub fn parse_bbo(
 ) -> Result<BboMsg, SimpleError> {
     match exchange {
         "binance" => exchanges::binance::parse_bbo(market_type, msg, received_at),
-        "deribit" => exchanges::deribit::parse_bbo(market_type, msg, received_at),
+        "deribit" => exchanges::deribit::parse_bbo(market_type, msg),
         "kraken" => exchanges::kraken::parse_bbo(market_type, msg, received_at),
         _ => Err(SimpleError::new(format!("Unknown exchange {}", exchange))),
     }
