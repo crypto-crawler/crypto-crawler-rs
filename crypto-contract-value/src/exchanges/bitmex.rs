9fb8ffa512dc34b280efd86b33638b21ce385d98
soulmachine <soulmachine@gmail.com>
2022-08-07 03:54:22 -0700

    Fixed bitmex contract values

--- crypto-contract-value/src/exchanges/bitmex.rs
+++ crypto-contract-value/src/exchanges/bitmex.rs
@@ -7,86 +7,43 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 // key = market_type + pair
-static CONTRACT_VALUES: Lazy<HashMap<String, i64>> = Lazy::new(|| {
+static CONTRACT_VALUES: Lazy<HashMap<String, f64>> = Lazy::new(|| {
     // offline data, in case the network is down
-    let mut m: HashMap<String, i64> = vec![
-        ("inverse_future.BTC/USD", 100000000),
-        ("inverse_future.ETH/USD", 1000000000),
-        ("inverse_swap.BTC/EUR", 100000000),
-        ("inverse_swap.BTC/USD", 100000000),
-        ("inverse_swap.ETH/USD", 1000000000),
-        ("linear_future.ADA/BTC", 1000000),
-        ("linear_future.BTC/USDT", 1),
-        ("linear_future.ETH/BTC", 1000),
-        ("linear_future.ETH/USDT", 10),
-        ("linear_future.XRP/BTC", 1000000),
-        ("linear_swap.ADA/USDT", 10000),
-        ("linear_swap.ALTMEXT/USDT", 100),
-        ("linear_swap.APE/USDT", 1000),
-        ("linear_swap.AVAX/USDT", 100),
-        ("linear_swap.BCH/USDT", 10),
-        ("linear_swap.BNB/USDT", 100),
-        ("linear_swap.BTC/USDT", 1),
-        ("linear_swap.DEFIMEXT/USDT", 100),
-        ("linear_swap.DOGE/USDT", 10000),
-        ("linear_swap.DOT/USDT", 1000),
-        ("linear_swap.EOS/USDT", 1000),
-        ("linear_swap.ETH/USDT", 10),
-        ("linear_swap.FTM/USDT", 10000),
-        ("linear_swap.GAL/USDT", 1000),
-        ("linear_swap.GMT/USDT", 1000),
-        ("linear_swap.LINK/USDT", 1000),
-        ("linear_swap.LTC/USDT", 100),
-        ("linear_swap.LUNA/USDT", 1000),
-        ("linear_swap.MANA/USDT", 1000),
-        ("linear_swap.MATIC/USDT", 10000),
-        ("linear_swap.METAMEXT/USDT", 100),
-        ("linear_swap.NEAR/USDT", 1000),
-        ("linear_swap.OP/USDT", 1000),
-        ("linear_swap.SAND/USDT", 1000),
-        ("linear_swap.SHIB/USDT", 1000000),
-        ("linear_swap.SOL/USDT", 100),
-        ("linear_swap.TRX/USDT", 100000),
-        ("linear_swap.USDBRL/BTC", 10000),
-        ("linear_swap.XRP/USDT", 10000),
-        ("quanto_future.ETH/USD", 100),
-        ("quanto_swap.ADA/USD", 10000),
-        ("quanto_swap.APE/USD", 1000),
-        ("quanto_swap.AVAX/USD", 1000),
-        ("quanto_swap.AXS/USD", 100),
-        ("quanto_swap.BCH/USD", 100),
-        ("quanto_swap.BNB/USD", 100),
-        ("quanto_swap.DOGE/USD", 100000),
-        ("quanto_swap.DOT/USD", 1000),
-        ("quanto_swap.EOS/USD", 10000),
-        ("quanto_swap.ETH/USD", 100),
-        ("quanto_swap.EUR/CHF", 10000),
-        ("quanto_swap.EUR/USD", 10000),
-        ("quanto_swap.EUR/USDT", 1000000),
-        ("quanto_swap.GAL/USD", 10000),
-        ("quanto_swap.GMT/USD", 10000),
-        ("quanto_swap.LINK/USD", 1000),
-        ("quanto_swap.LTC/USD", 200),
-        ("quanto_swap.LUNA/USD", 10000),
-        ("quanto_swap.NEAR/USD", 10000),
-        ("quanto_swap.NZD/USD", 10000),
-        ("quanto_swap.NZD/USDT", 1000000),
-        ("quanto_swap.OP/USD", 10000),
-        ("quanto_swap.SOL/USD", 100),
-        ("quanto_swap.TRX/USD", 100000),
-        ("quanto_swap.USD/CHF", 10000),
-        ("quanto_swap.USD/CNH", 10000),
-        ("quanto_swap.USD/INR", 10000),
-        ("quanto_swap.USD/MXN", 10000),
-        ("quanto_swap.USD/SEK", 10000),
-        ("quanto_swap.USD/TRY", 10000),
-        ("quanto_swap.USDT/CHF", 1000000),
-        ("quanto_swap.USDT/CNH", 1000000),
-        ("quanto_swap.USDT/MXN", 1000000),
-        ("quanto_swap.USDT/SEK", 1000000),
-        ("quanto_swap.USDT/TRY", 1000000),
-        ("quanto_swap.USDT/ZAR", 1000000),
-        ("quanto_swap.XRP/USD", 20000),
+    let mut m: HashMap<String, f64> = vec![
+        ("inverse_future.BTC/USD", 1.0),
+        ("inverse_swap.BTC/EUR", 1.0),
+        ("inverse_swap.BTC/USD", 1.0),
+        ("inverse_swap.ETH/USD", 1.0),
+        ("linear_future.ADA/BTC", 0.01),
+        ("linear_future.ETH/BTC", 0.00001),
+        ("linear_future.ETH/USDT", 0.00001),
+        ("linear_future.XRP/BTC", 0.01),
+        ("linear_swap.ADA/USDT", 0.01),
+        ("linear_swap.APE/USDT", 0.001),
+        ("linear_swap.AVAX/USDT", 0.0001),
+        ("linear_swap.BCH/USDT", 0.00001),
+        ("linear_swap.BNB/USDT", 0.0001),
+        ("linear_swap.BTC/USDT", 0.000001),
+        ("linear_swap.DEFIMEXT/USDT", 0.0001),
+        ("linear_swap.DOGE/USDT", 0.01),
+        ("linear_swap.DOT/USDT", 0.001),
+        ("linear_swap.EOS/USDT", 0.001),
+        ("linear_swap.ETH/USDT", 0.00001),
+        ("linear_swap.FTM/USDT", 0.01),
+        ("linear_swap.GAL/USDT", 0.001),
+        ("linear_swap.GMT/USDT", 0.001),
+        ("linear_swap.LINK/USDT", 0.001),
+        ("linear_swap.LTC/USDT", 0.0001),
+        ("linear_swap.LUNA/USDT", 0.001),
+        ("linear_swap.MANA/USDT", 0.001),
+        ("linear_swap.MATIC/USDT", 0.01),
+        ("linear_swap.NEAR/USDT", 0.001),
+        ("linear_swap.OP/USDT", 0.001),
+        ("linear_swap.SAND/USDT", 0.001),
+        ("linear_swap.SHIB/USDT", 1.0),
+        ("linear_swap.SOL/USDT", 0.0001),
+        ("linear_swap.TRX/USDT", 0.1),
+        ("linear_swap.XRP/USDT", 0.01),
     ]
     .into_iter()
     .map(|x| (x.0.to_string(), x.1))
@@ -107,7 +64,7 @@ struct Instrument {
     state: String,
     typ: String,
     quoteCurrency: String,
-    multiplier: i64,
+    multiplier: f64,
     isQuanto: bool,
     isInverse: bool,
     hasLiquidity: bool,
@@ -116,12 +73,15 @@ struct Instrument {
     volume24h: i64,
     turnover: i64,
     turnover24h: i64,
+    underlyingToSettleMultiplier: Option<f64>,
+    underlyingToPositionMultiplier: Option<f64>,
+    quoteToSettleMultiplier: Option<f64>,
     #[serde(flatten)]
     extra: HashMap<String, Value>,
 }
 
-fn fetch_contract_values() -> BTreeMap<String, i64> {
-    let mut mapping: BTreeMap<String, i64> = BTreeMap::new();
+fn fetch_contract_values() -> BTreeMap<String, f64> {
+    let mut mapping: BTreeMap<String, f64> = BTreeMap::new();
 
     if let Ok(text) = http_get("https://www.bitmex.com/api/v1/instrument/active") {
         let instruments: Vec<Instrument> = serde_json::from_str::<Vec<Instrument>>(&text)
@@ -130,16 +90,18 @@ fn fetch_contract_values() -> BTreeMap<String, i64> {
             .filter(|x| x.state == "Open" && x.hasLiquidity && x.volume24h > 0 && x.turnover24h > 0)
             .collect();
 
-        for instrument in instruments.iter() {
+        for instrument in instruments
+            .iter()
+            .filter(|instrument| !instrument.isQuanto && instrument.typ != "IFXXXP")
+        {
             let market_type = crypto_pair::get_market_type(&instrument.symbol, "bitmex", None);
             let pair = crypto_pair::normalize_pair(&instrument.symbol, "bitmex").unwrap();
             mapping.insert(
                 market_type.to_string() + "." + pair.as_str(),
-                if instrument.isInverse {
-                    debug_assert!(instrument.multiplier < 0);
-                    instrument.multiplier.abs()
+                if let Some(x) = instrument.underlyingToSettleMultiplier {
+                    instrument.multiplier / x
                 } else {
-                    instrument.multiplier
+                    instrument.multiplier / instrument.quoteToSettleMultiplier.unwrap()
                 },
             );
         }
@@ -154,7 +116,7 @@ pub(crate) fn get_contract_value(market_type: MarketType, pair: &str) -> Option<
     }
     let key = market_type.to_string() + "." + pair;
     if CONTRACT_VALUES.contains_key(key.as_str()) {
-        Some(CONTRACT_VALUES[&key] as f64 * 1e-8)
+        Some(CONTRACT_VALUES[&key])
     } else {
         Some(1.0)
     }
@@ -162,17 +124,23 @@ pub(crate) fn get_contract_value(market_type: MarketType, pair: &str) -> Option<
 
 #[cfg(test)]
 mod tests {
+    use std::collections::BTreeMap;
+
     use super::fetch_contract_values;
 
     #[ignore]
     #[test]
     fn test_fetch_contract_values() {
-        let mut mapping = fetch_contract_values();
+        let new_data = fetch_contract_values();
+
+        let mut mapping: BTreeMap<String, f64> = BTreeMap::new();
         for (key, value) in super::CONTRACT_VALUES.iter() {
-            if !mapping.contains_key(key) {
-                mapping.insert(key.to_string(), *value);
-            }
+            mapping.insert(key.to_string(), *value);
         }
+        for (key, value) in new_data.iter() {
+            mapping.insert(key.to_string(), *value);
+        }
+
         for (pair, contract_value) in &mapping {
             println!("(\"{pair}\", {contract_value}),");
         }
